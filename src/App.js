c1388bf96a7c3edeeeb1da7b2b949ab41f993237
Eliks <20fantomas09@gmail.com>
2024-04-01 21:00:28 +0400

    Test
    First commit :)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+import logo_html from './html.png'
+import logo_css from './css.png' 
+import logo_js from './js.png'
+import logo_react from './logo.svg';
+import logo_copy from './copy.png'
 import './App.css';
 
 function App() {
+  const encrypt = () => {
+    let text = document.getElementsByClassName("input_text")[0].value;
+    let len = text.length;
+    var result_array = [];
+    for (let i = 0; i < len; i++) {
+      let b = String.fromCharCode(text.charCodeAt(i) + 2);
+      result_array.push(b);
+    }
+    document.getElementsByClassName("p_outputP")[0].innerText = result_array.join('');
+  }
+  
+  const decrypt = () => {
+    let text = document.getElementsByClassName("input_text")[0].value;
+    let len = text.length;
+    var result_array = [];
+    for (let i = 0; i < len; i++) {
+      let b = String.fromCharCode(text.charCodeAt(i) - 2);
+      result_array.push(b);
+    }
+    document.getElementsByClassName("p_output")[0].innerText = result_array.join('');
+  }
+  
+  const copy = () => {
+    let text_output = document.getElementsByClassName("p_output")[0].innerText;
+    navigator.clipboard.writeText(text_output);
+    alert("Copied succesfully!");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="title">encrypt it</h1>
       </header>
+
+      <main className="main">
+      <input type="textarea" className="input_text" name="text" placeholder="Enter the text..." />
+      <br></br>
+      <div className="buttons">
+        <button className="encrypt_button" type="button" onClick={encrypt}>Encrypt</button>
+        <button className="decrypt_button" type="button" onClick={decrypt}>Decrypt</button>
+      </div>
+      
+      <div className="text_output_container">
+
+        <div className="copy_panel">
+        <button className="copy_btn" onClick={copy}><img src={logo_copy} className="copy_img" alt="copy" /></button>
+        </div>
+        
+        <div className="text_output">
+          <p className="p_output">Encrypted or decrypted text will appear here...</p>
+        </div>
+
+      </div>
+      </main>
+
+      <footer>
+      <h5 className="made_using">Made using</h5>
+      <div className="made_using_icons">
+        <img src={logo_html} className="html" alt="html" />
+        <img src={logo_css} className="css" alt="html" />
+        <img src={logo_js} className="js" alt="js" />
+        <img src={logo_react} className="React" alt="react" />
+
+      </div>
+      </footer>
+
     </div>
   );
 }
